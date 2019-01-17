805783ca5979e63c22484265fe9d640e56d4e83a
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-01-17 13:54:04 -0600

    added function displayGifs and ajax call

--- assets/js/giphy-app.js
+++ assets/js/giphy-app.js
@@ -30,7 +30,20 @@ $(document).ready(function () {
         });
     }
 
-    
+    // display the gifs
+
+    function displayGifs() {
+        var userInput = $(this).attr('data-topic');
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        userInput + "&api_key=wqWPwnwvmTeWthIWN0Hl90tZizhktHfj&limit=10";
+
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }). then(function (response) {
+            
+        }
+    }
 
 
 });
