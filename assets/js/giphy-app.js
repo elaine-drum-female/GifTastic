15031dfb417226f0e5a1f477cb4e389ecdbc4b39
Drum EL <40779904+elaine-drum-female@users.noreply.github.com>
2019-01-17 13:37:37 -0600

    created the createGifButton function

--- assets/js/giphy-app.js
+++ assets/js/giphy-app.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
     // variables
     var topicBtnArray = ["computers", "music", "guitar", "drums", "the masked singer", "taylor swift"];
+
+    // create gif button
+
+    function createGifButton() {
+        $('#topicContainer').empty();
+        for(var i = 0; i < topicBtnArray.length;i++) {
+            var uniqueButton = $("<button>");
+            uniqueButton.addClass('newTopics');
+            uniqueButton.attr('data-topic', topicBtnArray[i]);
+            uniqueButton.text(topicBtnArray[i]);
+            $('#topicContainer').append(uniqueButton);
+        }
+    }
+
+    
 });
 
 
